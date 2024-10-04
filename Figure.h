7505ae01968e51a7eefc0877fe452d3201f5a32d
O-Danylchuk <odanylchuk@kse.org.ua>
2024-10-04 17:27:17 +0300

    updated Figure interface

--- Figure.h
+++ Figure.h
@@ -1,18 +1,27 @@
-
-struct Point
-{
-    int x;
-    int y;
-};
+#pragma once
 
 class Figure
 {
-private:
-    Point position;
+protected:
+    std::pair<int, int> position;
+
 public:
+    Figure(const std::pair<int, int>& position) : position(position) {}
+
+    virtual ~Figure() = default;
+
     virtual void draw() = 0;
-    virtual void move(int x, int y) = 0;  
 
-    Figure(Point position);
+    virtual void move(int x, int y)
+    {
+        position.first = x;
+        position.second = y;
+    }
+
+    enum class FigureType 
+    {
+        Circle,
+        Square,
+        Triangle
+    };
 };
-
