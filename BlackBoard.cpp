93980c15cfae9637d37340b2d45ba71607953b6f
O-Danylchuk <odanylchuk@kse.org.ua>
2024-10-06 19:23:55 +0300

    added methods to list all the figures, draw them and changed grid to nested vector

--- BlackBoard.cpp
+++ BlackBoard.cpp
@@ -1,15 +1,11 @@
 #include "BlackBoard.h"
 #include <iostream>
+#include "Circle.h"
+#include "Square.h"
+#include "Triangle.h"
 
-BlackBoard::BlackBoard(int height, int width) : m_height(height), m_width(width) {
-    m_board = new char*[m_height];
-    for (int i = 0; i < m_height; ++i) {
-        m_board[i] = new char[m_width];
-        for (int j = 0; j < m_width; ++j) {
-            m_board[i][j] = ' ';
-        }
-    }
-}
+
+BlackBoard::BlackBoard(int height, int width) : m_height(height), m_width(width), m_board(height, std::vector<char>(width, ' ')) {}
 
 void BlackBoard::display() const {
     std::cout << "+";
@@ -33,17 +29,96 @@ void BlackBoard::display() const {
     std::cout << "+" << std::endl;
 }
 
-BlackBoard::~BlackBoard() {
-    for (int i = 0; i < m_height; ++i) {
-        delete[] m_board[i];
+void BlackBoard::drawFigure(Figure::FigureType type, std::pair<int, int>& position, int firstVal, int secondVal) {
+    if (type == Figure::FigureType::Circle) {
+        int radius = firstVal;
+        int centerX = position.first;
+        int centerY = position.second;
+
+        for (int y = -radius; y <= radius; ++y) {
+            for (int x = -radius; x <= radius; ++x) {
+                if (x * x + y * y <= radius * radius) {
+                    int drawX = centerX + x;
+                    int drawY = centerY + y;
+                    if (drawX >= 0 && drawX < m_width && drawY >= 0 && drawY < m_height) {
+                        m_board[drawY][drawX] = 'O';
+                    }
+                }
+            }
+        }
+    } else if (type == Figure::FigureType::Square) {
+        int height = firstVal;
+        int width = secondVal;
+        int startX = position.first;
+        int startY = position.second;
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                int drawX = startX + x;
+                int drawY = startY + y;
+                if (drawX >= 0 && drawX < m_width && drawY >= 0 && drawY < m_height) {
+                    m_board[drawY][drawX] = 'X';
+                }
+            }
+        }
+    } else if (type == Figure::FigureType::Triangle) {
+        int height = firstVal;
+        int startX = position.first;
+        int startY = position.second;
+        for (int x = 1; x <= height; ++x) {
+            for (int y = 0; y < x; ++y) {
+                int drawX = startX + x;
+                int drawY = startY + y;
+                if (drawX >= 0 && drawX < m_width && drawY >= 0 && drawY < m_height) {
+                    m_board[drawY][drawX] = 'X';
+                }
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Invalid figure type" << std::endl;
     }
-    delete[] m_board;
+}
+
+void BlackBoard::addFigure(std::unique_ptr<Figure>& figure){
+    m_figures.push_back(std::move(figure));
 }
 
 void BlackBoard::clearBoard() {
-    for (int i = 0; i < m_height; ++i) {
-        for (int j = 0; j < m_width; ++j) {
-            m_board[i][j] = ' ';
+    for (auto& row : m_board) {
+        std::fill(row.begin(), row.end(), ' ');
+    }
+}
+
+void BlackBoard::listFigures() {
+    for (const auto& figure : m_figures) {
+        switch (figure->type) {
+            case Figure::FigureType::Circle: {
+                Circle* circle = dynamic_cast<Circle*>(figure.get());
+                if (circle) {
+                    std::cout << "Circle: Position (" << circle->getPosition().first << ", " << circle->getPosition().second 
+                              << "), Radius: " << circle->getRadius() << std::endl;
+                }
+                break;
+            }
+            case Figure::FigureType::Square: {
+                Square* square = dynamic_cast<Square*>(figure.get());
+                if (square) {
+                    std::cout << "Square: Position (" << square->getPosition().first << ", " << square->getPosition().second 
+                              << "), Width: " << square->getWidth() << ", Height: " << square->getHeight() << std::endl;
+                }
+                break;
+            }
+            case Figure::FigureType::Triangle: {
+                Triangle* triangle = dynamic_cast<Triangle*>(figure.get());
+                if (triangle) {
+                    std::cout << "Triangle: Position (" << triangle->getPosition().first << ", " << triangle->getPosition().second 
+                              << ")" << ", Height: " << triangle->getHeight() << std::endl;
+                }
+                break;
+            }
+            default:
+                break;
         }
     }
 }
--- BlackBoard.h
+++ BlackBoard.h
@@ -1,13 +1,22 @@
+#pragma once
+#include "Figure.h"
+#include <vector>
+#include <memory>
+
 class BlackBoard
 {
 public:
     BlackBoard(int height, int width);
-    ~BlackBoard();
+    ~BlackBoard() = default;
 
     void display() const;
     void clearBoard();
+    void listFigures();
+    void addFigure(std::unique_ptr<Figure>& figure);
+    void drawFigure(Figure::FigureType type, std::pair<int, int>& position, int firstVal, int secondVal);
 private:
     int m_height;
     int m_width;
-    char** m_board;
+    std::vector<std::vector<char>> m_board;
+    std::vector<std::unique_ptr<Figure>> m_figures;
 };
